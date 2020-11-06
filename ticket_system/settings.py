fc08642b203f9a37ba196fa74642dbfaf99d3d73
AnnaMrowca <amrowca@gmail.com>
2020-11-06 21:50:39 +0100

    Changing language code from pl-pl to pl

--- ticket_system/settings.py
+++ ticket_system/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'pl-pl'
+LANGUAGE_CODE = 'pl'
 
 TIME_ZONE = 'Europe/Warsaw'
 
