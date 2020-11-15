c0666cbd986d535740422ea1f1aa95af8ee6de9b
AnnaMrowca <62843222+AnnaMrowca@users.noreply.github.com>
2020-11-15 17:23:43 +0100

    Fix od brancha proba (#4)
    * zmiany awaryjne
    
    * zmiana urls w ticket_system i bootstrapa w base
    
    * Major front page update
    
    Co-authored-by: Piotr Jewasinski <piterosjew@gmail.com>

--- ticket_system/urls.py
+++ ticket_system/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("event", include("event.urls")),
+    path("", include("event.urls")),
 
 ]
 
