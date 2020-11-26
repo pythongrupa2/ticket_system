3a4ff0628147134eccb6d7f666cf40078bf44dde
Piotr Jewasinski <piterosjew@gmail.com>
2020-11-26 18:38:08 +0100

    cleaning code, add superuser

--- event/models.py
+++ event/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django import forms
 from phone_field import PhoneField
 
 
@@ -14,6 +13,7 @@ class Event(models.Model):
     def __str__(self):
         return self.name_event, self.performer, self.date_event
 
+
 class User(models.Model):
     user_login = models.CharField(max_length=50, blank=False, null=False, verbose_name=("User Login"))
     user_email = models.CharField(max_length=100, blank=False, null=False, verbose_name=("User Email"))
@@ -21,6 +21,7 @@ class User(models.Model):
 
     def __str__(self):
         return self.user_login, self.user_email, self.user_phone
+
 
 class Payment(models.Model):
     payment_type = models.CharField(max_length=50, blank=False, null=False, verbose_name=("Type of Payment"))
@@ -33,7 +34,6 @@ class Payment(models.Model):
         return self.payment_type, self.date_payment, self.date_payment_return, self.payment_amount
 
 
-
 class Operator(models.Model):
     user_name = models.CharField(max_length=50, blank=False, null=False, verbose_name=("User Name"))
     user_surname = models.CharField(max_length=50, blank=False, null=False, verbose_name=("User Surname"))
@@ -41,27 +41,26 @@ class Operator(models.Model):
     def __str__(self):
         return self.user_name, self.user_surname
 
+
 TICKET_TYPE_CHOICES = [
     ("1", "Standard"),
     ("2", "VIP"),
 ]
 
+
 # class TicketType(forms.Form):
 #     ticket_type = forms.CharField(choices = TICKET_TYPE_CHOICES)
 
 class Ticket(models.Model):
-    ticket_type = models.CharField(choices = TICKET_TYPE_CHOICES, max_length=10)
+    ticket_type = models.CharField(choices=TICKET_TYPE_CHOICES, max_length=10)
     date_reservation = models.DateField(blank=True, null=True)
     date_purchase = models.DateField(blank=False, null=False)
     date_ticket_return = models.DateField(blank=True, null=True)
-    #foreign keys
-    event = models.ForeignKey(Event, on_delete=models.CASCADE) #co z related_name?????
+    # foreign keys
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)  # co z related_name?????
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
     user_ticket = models.ForeignKey(User, on_delete=models.CASCADE)
     operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.ticket_type, self.date_reservation, self.date_purchase, self.date_ticket_return
-
-
-
--- ticket_system/settings.py
+++ ticket_system/settings.py
@@ -13,7 +13,6 @@ import os
 
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #Anna
@@ -29,7 +28,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -77,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ticket_system.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -87,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -107,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -120,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
